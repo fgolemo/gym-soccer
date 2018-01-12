6a26aae3d7e53446370e9f5ecd0d6d291715ea97
Florian Golemo <fgolemo@gmail.com>
2018-01-11 21:27:41 -0500

    python 3 conversion of test

--- gym_vrep/envs/vrepper-test.py
+++ gym_vrep/envs/vrepper-test.py
@@ -7,7 +7,6 @@ venv = vrepper(headless=False)
 venv.start()
 venv.load_scene(os.getcwd() + '/../scenes/poppy_ergo_jr_vanilla_ball.ttt')
 
-
 motors = []
 for i in range(6):
     motor = venv.get_object_by_name('m{}'.format(i + 1), is_joint=True)
@@ -20,12 +19,12 @@ def reset():
     venv.stop_simulation()
     venv.start_nonblocking_simulation()
 
-    print "SETTING MOTORS"
+    print("SETTING MOTORS")
 
     for i, m in enumerate(motors):
         m.set_position_target(restMotors[i])
 
-    print "SPAWNING BALL"
+    print("SPAWNING BALL")
 
     params = venv.create_params(
         [],
@@ -36,9 +35,9 @@ def reset():
         ''
     )
     test = venv.call_script_function('spawnBall', params)
-    print test
+    print(test)
     time.sleep(.5)
-    print "MAKING SIM SYNC"
+    print("MAKING SIM SYNC")
 
     venv.make_simulation_synchronous(True)
 
@@ -48,25 +47,25 @@ def reset():
 throwStart = [0, 40, 40, 0, -90, -55]
 throwEnd = [0, -50, -40, 0, -50, -30]
 
+
 def gotoPos(pos):
     for i, m in enumerate(motors):
         m.set_position_target(pos[i])
 
 
-
 for i in range(2):
     reset()
     ball = venv.get_object_by_name("ball")
-    gotoPos(throwStart) # start pos
+    gotoPos(throwStart)  # start pos
     for _ in range(30):
         venv.step_blocking_simulation()
-        print ball.get_position()
-    gotoPos(throwEnd) # end pos
+        print(ball.get_position())
+    gotoPos(throwEnd)  # end pos
     forces = motors[0].get_joint_force()
-    print forces
+    print(forces)
     for _ in range(30):
         venv.step_blocking_simulation()
-        print ball.get_position()
+        print(ball.get_position())
     time.sleep(1)
 
 venv.stop_blocking_simulation()
